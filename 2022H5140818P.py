be698ceb53297632d4c5fb49eb3f49a897dfd17b
Aditya Anand Iyer <113101868+adityaiyer3@users.noreply.github.com>
2022-12-08 19:10:39 +0530

    Add files via upload

---
+++ 2022H5140818P.py
@@ -0,0 +1,12 @@
+def nth_root(a,b) :
+    if b == 0:
+        print("Please enter a valid no.")
+    else :
+        c= a^(1/b)
+    return (c)
+def nth_power(a,b) :
+    if a==0 and b==0 :
+        print("Indeterminate form!")
+    else :
+        return (a^b)
+
